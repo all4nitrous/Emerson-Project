324220e641ff098f5359356ba3550f97842d262c
Nicholas Richmond <114439546+all4nitrous@users.noreply.github.com>
2022-12-09 01:35:35 -0600

    Fixed nav issues

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -3,7 +3,7 @@ const CTA = () => {
   return (
     <div className='cta'>
         
-        <a href="#contact" className="btn btn-primary">Let's talk about Ralph Waldo Emerson</a>
+        <a href="#about" className="btn btn-primary">Let's talk about Ralph Waldo Emerson</a>
     </div>
   )
 }
