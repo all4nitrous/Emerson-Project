a0904f3256863459f0415bbec633882940a78e82
Nicholas Richmond <114439546+all4nitrous@users.noreply.github.com>
2022-12-09 00:28:13 -0600

    Working on writings section

--- src/components/about/Experience/Experience.jsx
+++ src/components/about/Experience/Experience.jsx
@@ -1,61 +1,20 @@
 import React from 'react'
 import './Experience.css'
-import {BsPatchCheckFill} from 'react-icons/bs'
+import {GiPineTree} from 'react-icons/gi'
 const Experience = () => {
   return (
     <section id="experience">
-      <h5>The Skills I Have</h5>
-      <h2>My Experience</h2>
+      <h5>Waldo's Writings</h5>
+      <h2>Into the Wilderness</h2>
     
     <div className="container experience_container">
       <div className="experience_frontend">
-          <h3>Frontend Development</h3>
+          <h3>Nature</h3>
           <div className="experience_content">
             <article className='experience_details'>
-              <BsPatchCheckFill className='experience_details-icon'/>
+              
               <div>
-              <h4>HTML</h4>
-              <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-
-            <article className='experience_details'>
-              <BsPatchCheckFill className='experience_details-icon'/>
-              <div>
-              <h4>React</h4>
-              <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-
-            <article className='experience_details'>
-              <BsPatchCheckFill className='experience_details-icon'/>
-              <div>
-              <h4>CSS</h4>
-              <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-
-            <article className='experience_details'>
-              <BsPatchCheckFill className='experience_details-icon'/>
-              <div>
-              <h4>JavaScript</h4>
-              <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-
-            <article className='experience_details'>
-              <BsPatchCheckFill className='experience_details-icon'/>
-              <div>
-              <h4>Bootstrap</h4>
-              <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-
-            <article className='experience_details'>
-              <BsPatchCheckFill className='experience_details-icon'/>
-              <div>
-              <h4>Tailwind</h4>
-              <small className='text-light'>Experienced</small>
+              <h4>Emerson could be classified by many as an intellectual. Despite Emerson's time in the church, Emerson sought to learn more about the world. The ultimate conclusion that Emerson came to was that rather than looking to god or directly to nature for the answers we seek, living life gives us the answers that we need. Emerson saw us humans as a key to the secrets between god and nature as we are their creations. In nature Emerson suggests a simbitoic relationship with nature in which rather than looking inwards for guidance we look to nature as a reflection of man. Emerson takes this even further stating that man deploys four components, commodity, beauty, language, and discipline of nature in order to achieve insight into the further universe.</h4>
               </div>
             </article>
 
@@ -63,21 +22,12 @@ const Experience = () => {
       </div>
       {/*END OF FRONTEND*/}
       <div className="experience_backend">
-        <h3>Backend Development</h3>
+        <h3>The Parallels</h3>
       <div className="experience_content">
             <article className='experience_details'>
-              <BsPatchCheckFill className='experience_details-icon'/>
+              
               <div>
               <h4>Node JS</h4>
-              <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-
-            <article className='experience_details'>
-              <BsPatchCheckFill className='experience_details-icon'/>
-              <div>
-              <h4>PHP</h4>
-              <small className='text-light'>Basic</small>
               </div>
             </article>
 
